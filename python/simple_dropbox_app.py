3b528a4bfafa171c0774277fea1750d38c81aadd
Timothy Hunter <tjhunter@eecs.berkeley.edu>
2013-11-20 07:29:16 -0800

    added example

--- python/simple_dropbox_app.py
+++ python/simple_dropbox_app.py
@@ -1,5 +1,8 @@
-__author__ = 'tjhunter'
-# Taken from the flask example
+# -*- coding: utf-8 -*-
+"""
+An example of Dropbox App linking with Flask.
+"""
+
 import os
 import posixpath
 
@@ -8,6 +11,11 @@ from flask import Flask, request, session, g, redirect, url_for, abort, \
      render_template, flash, _app_ctx_stack
 
 from dropbox.client import DropboxClient, DropboxOAuth2Flow
+
+# configuration
+DEBUG = True
+DATABASE = 'myapp.db'
+SECRET_KEY = 'development key'
 
 # Fill these in!
 DROPBOX_APP_KEY = 'o7jb6lfmya9cl1a'
@@ -23,6 +31,27 @@ try:
     os.makedirs(app.instance_path)
 except OSError:
     pass
+
+def init_db():
+    """Creates the database tables."""
+    with app.app_context():
+        db = get_db()
+        with app.open_resource("schema.sql", mode="r") as f:
+            db.cursor().executescript(f.read())
+        db.commit()
+
+
+def get_db():
+    """
+    Opens a new database connection if there is none yet for the current application context.
+    """
+    top = _app_ctx_stack.top
+    if not hasattr(top, 'sqlite_db'):
+        sqlite_db = sqlite3.connect(os.path.join(app.instance_path, app.config['DATABASE']))
+        sqlite_db.row_factory = sqlite3.Row
+        top.sqlite_db = sqlite_db
+
+    return top.sqlite_db
 
 def get_access_token():
     username = session.get('user')
@@ -46,6 +75,31 @@ def home():
         account_info = client.account_info()
         real_name = account_info["display_name"]
     return render_template('index.html', real_name=real_name)
+
+@app.route('/dropbox-auth-finish')
+def dropbox_auth_finish():
+    username = session.get('user')
+    if username is None:
+        abort(403)
+    try:
+        access_token, user_id, url_state = get_auth_flow().finish(request.args)
+    except DropboxOAuth2Flow.BadRequestException, e:
+        abort(400)
+    except DropboxOAuth2Flow.BadStateException, e:
+        abort(400)
+    except DropboxOAuth2Flow.CsrfException, e:
+        abort(403)
+    except DropboxOAuth2Flow.NotApprovedException, e:
+        flash('Not approved?  Why not, bro?')
+        return redirect(url_for('home'))
+    except DropboxOAuth2Flow.ProviderException, e:
+        app.logger.exception("Auth error" + e)
+        abort(403)
+    db = get_db()
+    data = [access_token, username]
+    db.execute('UPDATE users SET access_token = ? WHERE username = ?', data)
+    db.commit()
+    return redirect(url_for('home'))
 
 @app.route('/dropbox-auth-start')
 def dropbox_auth_start():
